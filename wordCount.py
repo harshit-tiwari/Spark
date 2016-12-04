b2626124fb5cf5b59cb84a8c709c88e45f9fd72f
Harshit Tiwari <harshit.tiwari2015@gmail.com>
2016-12-04 21:34:23 +0530

    Spark program in Python to calculate wordcount

---
+++ wordCount.py
@@ -0,0 +1,15 @@
+#A Spark script in python to display the wordcount of the input file 
+#Save thsi file as wordcount.py and then use the following command to run it
+# spark-submit --master local wordcount.py to run in local mode or
+# spark-submit --master yarn wordcount.py to run in yarn mode or
+
+
+from pyspark import SparkContext, SparkConf
+conf = SparkConf().setAppName("Wordcount")
+sc = SparkContext(conf=conf)
+
+file = sc.textFile("/user/cloudera/wordcount.txt")
+fileFlat = file.flatMap(lambda x: x.split(", "))
+fileMap = fileFlat.map(lambda x: (x,1))
+fileReduce = fileMap.reduceByKey(lambda x,y: x+y)
+fileReduce.saveAsTextFile("/user/cloudera/outputs/wordcount")
